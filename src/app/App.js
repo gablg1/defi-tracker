fbacea58788807d01177c23ec6187ed024611ee9
Gabriel Guimaraes <gabriel@brex.com>
2021-12-20 16:30:22 -0300

    not working

--- src/app/App.js
+++ src/app/App.js
@@ -8,78 +8,30 @@ import { withTranslation } from "react-i18next";
 import { Model, serialize } from './model';
 import { testAll } from './test';
 
-const Doc = Model.register('doc', class Doc extends Model {
+const ABI = Model.register('abi', class ABI extends Model {
   static properties = {
-    x: Number,
-    y: Number,
+    stringifiedJson: String,
   }
   constructor(json = {}) {
     super(json);
-    this.x = this.x || 3;
-    this.y = this.y || 4;
+    this.stringifiedJson = this.stringifiedJson || '[]';
   }
 });
 
-class App extends Component {
-  state = {}
-  componentDidMount() {
-    this.onRouteChanged();
-    testAll();
-  }
-  render () {
-    let headerComponent = !this.state.isFullPageLayout ? <Header/> : '';
-    let footerComponent = !this.state.isFullPageLayout ? <Footer/> : '';
-    return (
-      <div className="container-scroller">
-        { headerComponent }
-        <div className="container-fluid page-body-wrapper">
-          <div className="main-panel">
-            <div className="content-wrapper">
-              <AppRoutes/>
-            </div>
-            { footerComponent }
+function App(props) {
+  return (
+    <div className="container-scroller">
+      <Header />
+      <div className="container-fluid page-body-wrapper">
+        <div className="main-panel">
+          <div className="content-wrapper">
+            <AppRoutes/>
           </div>
+          <Footer />
         </div>
       </div>
-    );
-  }
-
-  componentDidUpdate(prevProps) {
-    if (this.props.location !== prevProps.location) {
-      this.onRouteChanged();
-    }
-  }
-
-  onRouteChanged() {
-    console.log("ROUTE CHANGED");
-    const { i18n } = this.props;
-    const body = document.querySelector('body');
-    if(this.props.location.pathname === '/layout/RtlLayout') {
-      body.classList.add('rtl');
-      i18n.changeLanguage('ar');
-    }
-    else {
-      body.classList.remove('rtl')
-      i18n.changeLanguage('en');
-    }
-    window.scrollTo(0, 0);
-    const fullPageLayoutRoutes = ['/user-pages/login-1', '/user-pages/login-2', '/user-pages/register-1', '/user-pages/register-2', '/user-pages/lockscreen', '/error-pages/error-404', '/error-pages/error-500', '/general-pages/landing-page'];
-    for ( let i = 0; i < fullPageLayoutRoutes.length; i++ ) {
-      if (this.props.location.pathname === fullPageLayoutRoutes[i]) {
-        this.setState({
-          isFullPageLayout: true
-        })
-        document.querySelector('.page-body-wrapper').classList.add('full-page-wrapper');
-        break;
-      } else {
-        this.setState({
-          isFullPageLayout: false
-        })
-        document.querySelector('.page-body-wrapper').classList.remove('full-page-wrapper');
-      }
-    }
-  }
-
+    </div>
+  );
 }
 
-export default withTranslation()(withRouter(App));
+export default withRouter(App);
