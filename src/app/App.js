b6d28b3781109625f655d8211c15edc69469908a
Gabriel Guimaraes <gabriel@brex.com>
2021-12-21 12:05:56 -0300

    example valueLink

--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, Component } from 'react';
+import React, { useReducer, useState, useEffect, Component } from 'react';
 import './App.scss';
 import Header from './shared/Header';
 import Footer from './shared/Footer';
@@ -31,23 +31,35 @@ const WorldState = Model.register('world-state', class WorldState extends Model 
 });
 
 function App(props) {
-  const [worldState, setWorldState] = useState(null);
+  const [worldState, setWorldState__onlyUseOnce] = useState(null);
   const [worldStateLoaded, setWorldStateLoaded] = useState(false);
+  const [ignored, forceUpdate] = useReducer(x => x + 1, 0);
 
+  const handleSave = () => {
+    localStorage.setItem('__serializedWorldState', JSON.stringify(worldState.serialize()));
+    forceUpdate();
+  }
+
+  const worldStateChange = (obj, attr) => (e) => {
+    obj[attr] = e.target.value;
+    handleSave();
+  };
+
+  // Load the world state upon page load
   useEffect(() => {
     const serializedState = JSON.parse(localStorage.getItem('__serializedWorldState') || '{}');
-    setWorldState(new WorldState(serializedState));
+    setWorldState__onlyUseOnce(new WorldState(serializedState));
     setWorldStateLoaded(true);
   }, [worldStateLoaded]);
 
-  useEffect(() => {
-    if (worldStateLoaded && worldState != null) {
-      localStorage.setItem('__serializedWorldState', JSON.stringify(worldState.serialize()));
-    }
-  }, [worldState]);
+  if (!worldStateLoaded) {
+    return <div>Loading...</div>
+  }
 
   return (
     <div className="container-scroller">
+      <input type="text" className="form-control" placeholder="Testing..." value={worldState.defaultAddr}
+        onChange={worldStateChange(worldState, 'defaultAddr')} />
       <Header />
       <div className="container-fluid page-body-wrapper">
         <div className="main-panel">
