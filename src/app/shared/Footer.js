9c88b6746456e93bfdeceb10502b0ae600eeecf1
Gabriel Guimaraes <gabriel@brex.com>
2021-12-22 17:41:11 -0300

    made by stringer

--- src/app/shared/Footer.js
+++ src/app/shared/Footer.js
@@ -5,9 +5,8 @@ class Footer extends Component {
   render () {
     return (
       <footer className="footer container">
-        <div className="d-sm-flex justify-content-center justify-content-sm-between">
-          <span className="text-muted text-center text-sm-left d-block d-sm-inline-block"><Trans>Copyright</Trans> © 2020 <a href="https://www.bootstrapdash.com/" target="_blank" rel="noopener noreferrer">BootstrapDash</a>. <Trans>All rights reserved</Trans>.</span>
-          <span className="text-muted float-none float-sm-right d-block mt-1 mt-sm-0 text-center"><Trans>Hand-crafted</Trans> & <Trans>made with</Trans> <i className="mdi mdi-heart text-danger"></i></span>
+        <div className="d-sm-flex justify-content-end">
+          <span className="text-muted float-none float-sm-right d-block mt-1 mt-sm-0 text-center"><Trans>Hand-crafted</Trans> & <Trans>made with</Trans> <i style={{padding: 0}} className="mdi mdi-heart text-danger"></i> by stringer</span>
         </div>
       </footer>
     );
